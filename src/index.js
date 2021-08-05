b75a63b563d49ddbb2e5b3229ff7a91f08caf43f
Yura Alexeev <yurals1014@gmail.com>
2021-08-05 21:31:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length === 0) return 0;
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length === 0) return 0;
+    return Math.max.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length === 0) return 0;
+    return array.reduce((a, b) => a + b) / array.length;
+};
+// console.log(avg([5, 12, 1, 130, 44]));
